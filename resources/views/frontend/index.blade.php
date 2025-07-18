7ab0614731cb681651a810ddab1a081341bae638
husnainmehboob189 <husnainmehboob189@gmail.com>
2025-07-18 14:41:08 +0500

    Initial commit

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,221 @@
+@extends('frontend.layout.main')
+@section('main-container')
+			<div class="main-panel">
+				<div class="content">
+					<div class="container-fluid">
+						<h4 class="page-title">Dashboard</h4>
+						<div class="row">
+							<div class="col-md-3">
+								<div class="card card-stats card-warning">
+									<div class="card-body ">
+										<div class="row">
+											<div class="col-5">
+												<div class="icon-big text-center">
+													<i class="la la-users"></i>
+												</div>
+											</div>
+											<div class="col-7 d-flex align-items-center">
+												<div class="numbers">
+													<p class="card-category">Visitors</p>
+													<h4 class="card-title">1,294</h4>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="card card-stats card-success">
+									<div class="card-body ">
+										<div class="row">
+											<div class="col-5">
+												<div class="icon-big text-center">
+													<i class="la la-bar-chart"></i>
+												</div>
+											</div>
+											<div class="col-7 d-flex align-items-center">
+												<div class="numbers">
+													<p class="card-category">Sales</p>
+													<h4 class="card-title">$ 1,345</h4>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="card card-stats card-danger">
+									<div class="card-body">
+										<div class="row">
+											<div class="col-5">
+												<div class="icon-big text-center">
+													<i class="la la-newspaper-o"></i>
+												</div>
+											</div>
+											<div class="col-7 d-flex align-items-center">
+												<div class="numbers">
+													<p class="card-category">Subscribers</p>
+													<h4 class="card-title">1303</h4>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="card card-stats card-primary">
+									<div class="card-body ">
+										<div class="row">
+											<div class="col-5">
+												<div class="icon-big text-center">
+													<i class="la la-check-circle"></i>
+												</div>
+											</div>
+											<div class="col-7 d-flex align-items-center">
+												<div class="numbers">
+													<p class="card-category">Order</p>
+													<h4 class="card-title">576</h4>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+<!-- 							<div class="col-md-3">
+								<div class="card card-stats">
+									<div class="card-body ">
+										<div class="row">
+											<div class="col-5">
+												<div class="icon-big text-center icon-warning">
+													<i class="la la-pie-chart text-warning"></i>
+												</div>
+											</div>
+											<div class="col-7 d-flex align-items-center">
+												<div class="numbers">
+													<p class="card-category">Number</p>
+													<h4 class="card-title">150GB</h4>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="card card-stats">
+									<div class="card-body ">
+										<div class="row">
+											<div class="col-5">
+												<div class="icon-big text-center">
+													<i class="la la-bar-chart text-success"></i>
+												</div>
+											</div>
+											<div class="col-7 d-flex align-items-center">
+												<div class="numbers">
+													<p class="card-category">Revenue</p>
+													<h4 class="card-title">$ 1,345</h4>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="card card-stats">
+									<div class="card-body">
+										<div class="row">
+											<div class="col-5">
+												<div class="icon-big text-center">
+													<i class="la la-times-circle-o text-danger"></i>
+												</div>
+											</div>
+											<div class="col-7 d-flex align-items-center">
+												<div class="numbers">
+													<p class="card-category">Errors</p>
+													<h4 class="card-title">23</h4>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="card card-stats">
+									<div class="card-body">
+										<div class="row">
+											<div class="col-5">
+												<div class="icon-big text-center">
+													<i class="la la-heart-o text-primary"></i>
+												</div>
+											</div>
+											<div class="col-7 d-flex align-items-center">
+												<div class="numbers">
+													<p class="card-category">Followers</p>
+													<h4 class="card-title">+45K</h4>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div> -->
+						</div>
+						<div class="row">
+							<div class="col-md-3">
+								<div class="card">
+									<div class="card-header">
+										<h4 class="card-title">Task</h4>
+										<p class="card-category">Complete</p>
+									</div>
+									<div class="card-body">
+										<div id="task-complete" class="chart-circle mt-4 mb-3"></div>
+									</div>
+									<div class="card-footer">
+										<div class="legend"><i class="la la-circle text-primary"></i> Completed</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-9">
+								<div class="card">
+									<div class="card-header">
+										<h4 class="card-title">World Map</h4>
+										<p class="card-category">
+										Map of the distribution of users around the world</p>
+									</div>
+									<div class="card-body">
+										<div class="mapcontainer">
+											<div class="map">
+												<span>Alternative content for the map</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-4">
+								<div class="card">
+									<div class="card-header">
+										<h4 class="card-title">Users Statistics</h4>
+										<p class="card-category">
+										Users statistics this month</p>
+									</div>
+									<div class="card-body">
+										<div id="monthlyChart" class="chart chart-pie"></div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-8">
+								<div class="card">
+									<div class="card-header">
+										<h4 class="card-title">2018 Sales</h4>
+										<p class="card-category">
+										Number of products sold</p>
+									</div>
+									<div class="card-body">
+										<div id="salesChart" class="chart"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			@endsection
---
+++ resources/views/frontend/layout/footer.blade.php
@@ -0,0 +1,90 @@
+	<footer class="footer">
+					<div class="container-fluid">
+						<nav class="pull-left">
+							<ul class="nav">
+								<li class="nav-item">
+									<a class="nav-link" href="http://www.themekita.com">
+										ThemeKita
+									</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">
+										Help
+									</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="https://themewagon.com/license/#free-item">
+										Licenses
+									</a>
+								</li>
+							</ul>
+						</nav>
+						<div class="copyright ml-auto">
+							2018, made with <i class="la la-heart heart text-danger"></i> by <a href="http://www.themekita.com">ThemeKita</a>
+						</div>				
+					</div>
+				</footer>
+			</div>
+		</div>
+	</div>
+	<!-- Modal -->
+	<div class="modal fade" id="modalUpdate" tabindex="-1" role="dialog" aria-labelledby="modalUpdatePro" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content">
+				<div class="modal-header bg-primary">
+					<h6 class="modal-title"><i class="la la-frown-o"></i> Under Development</h6>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body text-center">									
+					<p>Currently the pro version of the <b>Ready Dashboard</b> Bootstrap is in progress development</p>
+					<p>
+						<b>We'll let you know when it's done</b></p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+<!-- jQuery Core -->
+<script src="{{ asset('frontend/js/core/jquery.3.2.1.min.js') }}"></script>
+
+<!-- jQuery UI -->
+<script src="{{ asset('frontend/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js') }}"></script>
+
+<!-- Popper -->
+<script src="{{ asset('frontend/js/core/popper.min.js') }}"></script>
+
+<!-- Bootstrap -->
+<script src="{{ asset('frontend/js/core/bootstrap.min.js') }}"></script>
+
+<!-- Chartist -->
+<script src="{{ asset('frontend/js/plugin/chartist/chartist.min.js') }}"></script>
+<script src="{{ asset('frontend/js/plugin/chartist/plugin/chartist-plugin-tooltip.min.js') }}"></script>
+
+<!-- Bootstrap Notify -->
+<script src="{{ asset('frontend/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
+
+<!-- Bootstrap Toggle -->
+<script src="{{ asset('frontend/js/plugin/bootstrap-toggle/bootstrap-toggle.min.js') }}"></script>
+
+<!-- jQuery Mapael -->
+<script src="{{ asset('frontend/js/plugin/jquery-mapael/jquery.mapael.min.js') }}"></script>
+<script src="{{ asset('frontend/js/plugin/jquery-mapael/maps/world_countries.min.js') }}"></script>
+
+<!-- Chart Circle -->
+<script src="{{ asset('frontend/js/plugin/chart-circle/circles.min.js') }}"></script>
+
+<!-- jQuery Scrollbar -->
+<script src="{{ asset('frontend/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
+
+<!-- Ready Admin Template Script -->
+<script src="{{ asset('frontend/js/ready.min.js') }}"></script>
+
+<!-- Demo Script -->
+<script src="{{ asset('frontend/js/demo.js') }}"></script>
+
+</html>
